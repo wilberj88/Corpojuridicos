5d9e45cec473c577ef3695f6e9bd70fc4cb4d14d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-12-02 13:52:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,32 @@ import streamlit as st
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Corpojurídicos", page_icon="⚖️")
+
+#TITULO
+st.title('Corpojuridicos ⚖️ - Central de Atención Jurídica')
+
+#SUBTITULO
+st.write('---')
+st.write("""
+**Nueve (9) Centrales de Mando para sus Casos Jurídicos:**
+- 🗺: `Necesidades Territoriales`
+- 💛: `Sentimientos Digitales`
+- 🧭: `Propuestas`
+- 🚧: `Proyectos`
+- 🗣️: `Voluntarios`
+- 🦶: `Líderes`
+- 🧑‍⚖️: `Jurados`
+- 🕵️: `Testigos`
+- 💰: `Financiación`
+""")
+st.write("""
+**Sistema de Alarmas para:**
+- ⏰ : `Retrasos en Cumplimientos de Metas`
+""")
+st.write("""
+**Sistema de Recomendación para:**
+- 📈:  `Más Votos, Influencia y Sentimientos favorables`
+""")
+st.write('---')
+st.markdown('Versión Ejemplo Borrador - Cotiza tu versión personalizada en www.novusvote.com')
+
