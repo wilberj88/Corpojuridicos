57905cf92ce7f4f74615f7e4a12af69f63b4159f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-12-02 13:31:35 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
+import altair as alt
+import pydeck as pdk
+import matplotlib.pyplot as plt
+import datetime
+import base64
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Vote 🗳️", page_icon="🗳️")
