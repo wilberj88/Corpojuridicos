ac6513998bab31587386ed39c427bb445e997ee7
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-12-02 13:48:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
-import numpy as np
-import plotly.figure_factory as ff
-import altair as alt
-import pydeck as pdk
-import datetime
-import base64
+
 
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
